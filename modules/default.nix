192af81449be49acffa141369e36a752d7d5284e
yzy-1 <50034950+yzy-1@users.noreply.github.com>
2022-12-17 18:59:11 +0800

    delete deprecated packages (v2raya)

--- modules/default.nix
+++ modules/default.nix
@@ -1,6 +1,5 @@
 {
   # Add your NixOS modules here
   #
-  v2raya = ./services/v2raya.nix;
   # my-module = ./my-module;
 }
--- pkgs/cpt-fetcher/default.nix
+++ pkgs/cpt-fetcher/default.nix
@@ -2,17 +2,17 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "cpt-fetcher";
-  version = "c135993c585ff790de36a4cf54cd11135daca114";
+  version = "5414f55d8c406b2b632d4fd6b673b39f9ba2fb05";
   owner = "yzy-1";
 
   src = fetchFromGitHub {
     owner = owner;
     repo = pname;
     rev = version;
-    sha256 = "sha256-491/Y0Eo4K/24XyOVygUI9Y4pOP8lKfXndV8yFbZ4tw=";
+    sha256 = "sha256-Gt7Co0N2QUVd+8YauJx6iBD5EKG4OSDSk5qTCaWlqhQ=";
   };
 
-  cargoSha256 = "sha256-Pn8psTMOq2vLQafBEDwoio/OPNobWnAhj2mGu9qncac=";
+  cargoSha256 = "sha256-xMR95njYz10pmvwa+Vy/70Z1UrBTuAwUiAY+dXrDoYY=";
 
   meta = with lib; {
     description = "A bridge between Competitive Companion and cpt.";
